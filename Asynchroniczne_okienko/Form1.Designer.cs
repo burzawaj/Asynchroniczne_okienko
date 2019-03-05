ef8fcfe8906c8c8c782c41c36baf836e93531b15
Jaroslaw Burzawa <jaroslaw.burzawa@balluff.pl>
2019-03-05 07:23:53 +0100

    dołożono monitorowanie katalogu - na razie stały katalog

--- Asynchroniczne_okienko/Form1.Designer.cs
+++ Asynchroniczne_okienko/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_dwa = new System.Windows.Forms.Button();
+            this.textBoxKomentarz = new System.Windows.Forms.TextBox();
+            this.textBox_ZEGAR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_WyborMonitorowanegoKatalogu = new System.Windows.Forms.Button();
+            this.listBox_LogiZmian = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button_dwa
+            // 
+            this.button_dwa.Location = new System.Drawing.Point(535, 82);
+            this.button_dwa.Name = "button_dwa";
+            this.button_dwa.Size = new System.Drawing.Size(171, 54);
+            this.button_dwa.TabIndex = 0;
+            this.button_dwa.Text = "button1";
+            this.button_dwa.UseVisualStyleBackColor = true;
+            this.button_dwa.Click += new System.EventHandler(this.button_dwa_Click);
+            // 
+            // textBoxKomentarz
+            // 
+            this.textBoxKomentarz.Location = new System.Drawing.Point(535, 166);
+            this.textBoxKomentarz.Multiline = true;
+            this.textBoxKomentarz.Name = "textBoxKomentarz";
+            this.textBoxKomentarz.Size = new System.Drawing.Size(253, 243);
+            this.textBoxKomentarz.TabIndex = 1;
+            // 
+            // textBox_ZEGAR
+            // 
+            this.textBox_ZEGAR.Location = new System.Drawing.Point(535, 45);
+            this.textBox_ZEGAR.Name = "textBox_ZEGAR";
+            this.textBox_ZEGAR.Size = new System.Drawing.Size(187, 22);
+            this.textBox_ZEGAR.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(532, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zegar z datą";
+            // 
+            // button_WyborMonitorowanegoKatalogu
+            // 
+            this.button_WyborMonitorowanegoKatalogu.Location = new System.Drawing.Point(24, 27);
+            this.button_WyborMonitorowanegoKatalogu.Name = "button_WyborMonitorowanegoKatalogu";
+            this.button_WyborMonitorowanegoKatalogu.Size = new System.Drawing.Size(192, 58);
+            this.button_WyborMonitorowanegoKatalogu.TabIndex = 4;
+            this.button_WyborMonitorowanegoKatalogu.Text = "Wybierz monitorowany katalog";
+            this.button_WyborMonitorowanegoKatalogu.UseVisualStyleBackColor = true;
+            this.button_WyborMonitorowanegoKatalogu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox_LogiZmian
+            // 
+            this.listBox_LogiZmian.FormattingEnabled = true;
+            this.listBox_LogiZmian.ItemHeight = 16;
+            this.listBox_LogiZmian.Location = new System.Drawing.Point(24, 106);
+            this.listBox_LogiZmian.Name = "listBox_LogiZmian";
+            this.listBox_LogiZmian.Size = new System.Drawing.Size(446, 292);
+            this.listBox_LogiZmian.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox_LogiZmian);
+            this.Controls.Add(this.button_WyborMonitorowanegoKatalogu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_ZEGAR);
+            this.Controls.Add(this.textBoxKomentarz);
+            this.Controls.Add(this.button_dwa);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_dwa;
+        private System.Windows.Forms.TextBox textBoxKomentarz;
+        private System.Windows.Forms.TextBox textBox_ZEGAR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_WyborMonitorowanegoKatalogu;
+        private System.Windows.Forms.ListBox listBox_LogiZmian;
     }
 }
 
--- Asynchroniczne_okienko/Form1.cs
+++ Asynchroniczne_okienko/Form1.cs
@@ -3,18 +3,155 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace Asynchroniczne_okienko
+
 {
+    delegate void StringArgReturningVoidDelegate(string text);
+    
+
     public partial class Form1 : Form
     {
+        private FileSystemWatcher _monitor;
+        private static System.Timers.Timer mojTimer;
         public Form1()
         {
             InitializeComponent();
+            InitWatcher();
+            _monitor.EnableRaisingEvents = true;
+            Thread watekpoboczny = new Thread(SprawdzCzas);
+            watekpoboczny.Start();
+            
+            
         }
+
+        private void SprawdzCzas()
+        {
+            mojTimer = new System.Timers.Timer(1000);
+            mojTimer.Elapsed += OnTimedEvent;
+            mojTimer.AutoReset = true;
+            mojTimer.Enabled = true;
+            
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            
+            SetText(e.SignalTime.ToString()); 
+        }
+
+        private void SetText(string text)
+        {
+            if (this.textBox_ZEGAR.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox_ZEGAR.Text = text;
+            }
+        }
+                          
+        private async void button_dwa_Click(object sender, EventArgs e)
+        {
+            string tekst = await ToDo();  //await powoduje zwrócenie od razu rezultatu zadania które jest wynikiem funkcji
+            textBoxKomentarz.AppendText(tekst + "\r\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog _wybierzFolder = new FolderBrowserDialog();
+            if (_wybierzFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK) MessageBox.Show(_wybierzFolder.SelectedPath);
+        }
+        
+        private void InitWatcher()
+        {
+            _monitor = new FileSystemWatcher(@"C:\Roboczy");
+
+            _monitor.Changed += _watcher_Changed;
+            _monitor.Created += _watcher_Changed;
+            _monitor.Deleted += _watcher_Changed;
+            _monitor.Renamed += _watcher_Changed;
+        }
+
+        private void _watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string log = " ";
+            switch (e.ChangeType)
+            {
+                case WatcherChangeTypes.Created:
+                    log = $"Utworzono plik: {e.Name}";
+                    break;
+                case WatcherChangeTypes.Deleted:
+                    log = $"Usunieto plik: {e.Name}";
+
+                    break;
+                case WatcherChangeTypes.Changed:
+                    log = $"Zmieniono plik: {e.Name}";
+                    log = log + DateTime.Now.ToString();
+
+                    break;
+                case WatcherChangeTypes.Renamed:
+
+                    log = $"Zmieniono nazwę pliku: {e.Name}";
+
+
+                    break;
+                case WatcherChangeTypes.All:
+                    break;
+                default:
+                    break;
+            }
+
+            AddLog(log); //wywołanie funcji dodającej wpis do ListBox
+
+
+
+        }
+
+
+
+
+        private void AddLog(string log)
+        {
+            if (InvokeRequired) //sprawdzenie czy do wykonania potrzeba być w wątku głównym
+            {
+                BeginInvoke((Action)(() =>
+                {
+                    listBox_LogiZmian.Items.Insert(0, log);
+                    //wyslijEmail(log);
+
+                }
+                    ));//lamda do przeniesienia sie do głównego wątku
+            }
+        }
+
+    
+
+
+        private Task<string> ToDo()
+        {
+
+            Task<string> zadanie = Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                return "Nacisnąłeś przycisk 3s temu";
+            });
+            return zadanie;
+                       
+
+        }
+
+        
+
+        
     }
 }
